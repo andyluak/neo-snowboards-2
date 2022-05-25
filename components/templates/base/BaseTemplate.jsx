2a100332b887298d2d92dc2a149ffc6be1df5c5f
Alex Tirim <alexandru.tirim.cft@jcrew.com>
2022-05-25 14:14:29 +0300

    build/added storybook & comp template

---
+++ components/templates/base/BaseTemplate.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import styles from './style.module.scss';
+
+function BaseTemplate() {
+  return (
+    <div className={styles['container']}>Hello World! </div>
+  )
+}
+
+export default BaseTemplate
---
+++ components/templates/base/BaseTemplate.stories.jsx
@@ -0,0 +1,10 @@
+import BaseTemplate from '/components/templates/base/BaseTemplate';
+
+export default {
+    title: 'templates/BaseTemplate',
+    component: BaseTemplate,
+}
+
+const Template = (args) => <BaseTemplate {...args} />
+
+export const Base = Template.bind({});
